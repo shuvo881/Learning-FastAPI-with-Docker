24fcdb121bc868854a6eef1d3b2e5447c8ff5c6f
shuvo <golammostofa10001@gamil.com>
2023-11-18 00:11:51 +0600

    cookie

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-"""
-    You can declare multiple path parameters and query parameters at the same time, FastAPI knows which is which.
+from typing import Annotated
 
-    And you don't have to declare them in any specific order.
-"""
-
-from fastapi import FastAPI
+from fastapi import Cookie, FastAPI
 
 app = FastAPI()
 
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item["q"] = q
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
+@app.get("/items/")
+async def read_items(ads_id: Annotated[str | None, Cookie()] = None):
+    return {"ads_id": ads_id}
