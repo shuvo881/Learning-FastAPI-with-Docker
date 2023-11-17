d5bf767a5f4817d633da7f117016ff4c9fe6eb3b
shuvo <golammostofa10001@gamil.com>
2023-11-17 22:55:24 +0600

    paramiter required or not

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-from enum import Enum
+# When you declare other function parameters that are not part of the path parameters,\
+# they are automatically interpreted as "query" parameters.
 
 from fastapi import FastAPI
 
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
 app = FastAPI()
 
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+@app.get("/items/")
+async def read_item(skip: int, limit: int = 10, val: None = None): # val is no required, skip is required, limit is defalt variable and no required
+    return fake_items_db[skip : skip + limit]
